1d0dd336f5d83f83116aa5e5c78e1fc780536158
Abel <abelareiza@gmail.com>
2020-06-10 21:59:43 -0500

    Django App created

---
+++ notas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Usuario(models.Model):
+  usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+  esta_activo = models.BooleanField(default=True)
+  
+  def __str__(self):
+    return self.user.username
